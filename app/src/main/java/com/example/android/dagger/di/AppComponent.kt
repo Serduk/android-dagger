af63a589572bf15947c98013e80b07a2b0bfd846
Serhii Serdiuk <serduksergey90@gmail.com>
2020-02-03 18:37:56 +0200

    https://codelabs.developers.google.com/codelabs/android-dagger/index.html?index=..%2F..index#9
    Inject to settings activity

--- app/src/main/java/com/example/android/dagger/di/AppComponent.kt
+++ app/src/main/java/com/example/android/dagger/di/AppComponent.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import com.example.android.dagger.login.LoginComponent
 import com.example.android.dagger.main.MainActivity
 import com.example.android.dagger.registration.RegistrationComponent
+import com.example.android.dagger.settings.SettingsActivity
 import dagger.BindsInstance
 import dagger.Component
 import javax.inject.Singleton
@@ -14,6 +15,7 @@ import javax.inject.Singleton
 interface AppComponent {
     // Classes that can be injected by this Component
     fun inject(activity: MainActivity)
+    fun inject(activity: SettingsActivity)
 
     // Expose RegistrationComponent factory from the graph
     fun registrationComponent(): RegistrationComponent.Factory
